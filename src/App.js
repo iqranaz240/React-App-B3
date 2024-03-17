85ded6e9cdf354a0ac3d798d9305acdbaca96e2f
Iqra Naz <48812001+iqranaz240@users.noreply.github.com>
2024-03-17 11:57:29 +0500

    list rendering in React

--- src/App.js
+++ src/App.js
@@ -3,10 +3,51 @@ import Expense from './components/Expense/Expense';
 import Card from './components/UI/Card';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: 'e5',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e6', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e7',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e8',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div className="App">
      <h1>Expense Tracker App</h1>
-     <Expense/>
+     <Expense items={expenses}/>
     </div>
   );
 }
--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem'
 
-export default function Expense() {
+export default function Expense(props) {
   return (
     <div>
-        <ExpenseItem date='Mar 3, 2024' title='Book' amount='Pkr 500' />
-        <ExpenseItem date='Mar 2, 2024' title='Dress' amount='Pkr 2500' />
-        <ExpenseItem date='Mar 1, 2024' title='Shoes' amount='Pkr 1200' />
-        <ExpenseItem date='Feb 28, 2024' title='Bag' amount='Pkr 1000' />
+      {
+        props.items.map((item)=>(
+          <ExpenseItem key={item.id} date={item.date.toLocaleString()} title={item.title} amount={item.amount} />
+        ))
+      }
     </div>
   )
 }
